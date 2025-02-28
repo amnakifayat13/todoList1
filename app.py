0c703a77eaf64d05f876853f37e879ddece42199
amnajee-art <aftabamna13@gmail.com>
2025-03-01 00:48:58 +0500

    more updated

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import time
 st.title("Todo List")
 # initialize the todo session_state
 if 'todo' not in st.session_state:
-    st.session_state["todo"] = []
-add = st.session_state["todo"]
+    st.session_state.todo = []
+add = st.session_state.todo
 
 # initialize the edit_index  session_state
 if "edit_index" not in st.session_state:
@@ -14,22 +14,22 @@ if "edit_index" not in st.session_state:
 
 # add input for task
 task = st.text_input("enter your Item here", key=f"add_task{add}")
-click = st.button("Add Items")
+
 
 # add items
-if click:
+if st.button("Add Items"):
     if task:
-        st.session_state["todo"].append(task)
+        st.session_state.todo.append(task)
         st.rerun()
 
-for idx, i in enumerate(st.session_state["todo"]):
+for idx, i in enumerate(st.session_state.todo):
     col1, col2,col3 = st.columns(3)
     with col1:
         st.write(idx,i)
     # add remove button with items 
     with col2:
         if st.button("❌",key=f"{idx}"):
-            st.session_state["todo"].pop(idx)
+            st.session_state.todo.pop(idx)
             st.rerun()
     # update items
     with col3:
@@ -39,17 +39,17 @@ for idx, i in enumerate(st.session_state["todo"]):
 
 if st.session_state.edit_index is not None:
     index = st.session_state.edit_index
-    new_task = st.text_input("Update task:", value=st.session_state["todo"][index])
+    new_task = st.text_input("Update task:", value=st.session_state.todo[index])
 
     if st.button("OK"):
-        st.session_state["todo"][index] = new_task
+        st.session_state.todo[index] = new_task
         st.session_state.edit_index = None  
         st.rerun()
-        
+
 # clear all items
 
 if st.button("Clear All items"):
-    st.session_state["todo"].clear()
+    st.session_state.todo.clear()
     st.rerun()
     
 
